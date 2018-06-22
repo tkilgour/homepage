a63fa06eb0cd6b47cf6970103b1d0080140ea580
Thomas Kilgour <t.kilgour@gmail.com>
2018-06-21 22:07:31 -0400

    remove unnecessary css

--- components/Header.vue
+++ components/Header.vue
@@ -3,22 +3,3 @@
     <h1 id="name" class="run-animation">Thomas<br />Kilgour</h1>
   </header>
 </template>
-
-<style scoped>
-  header {
-  text-align: center;
-}
-
-h1 {
-  display: inline-block;
-  font-family: 'Comfortaa', sans-serif;
-  font-size: calc(3em + 1vw);
-  font-weight: 400;
-  margin: 0;
-  padding: 30px 10px;
-  text-align: center;
-  color: #1FDA9A;
-  /* prevent iphone highlight overlay on tap */
-  -webkit-tap-highlight-color: rgba(0,0,0,0); 
-}
-</style>
